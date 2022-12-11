6221d02318234b6693fd0b6eaa1c8e287a0bfd3c
ejsiik <72543889+ejsiik@users.noreply.github.com>
2022-12-11 13:36:25 +0100

    Nowa wersja aplikacji
    Do zmiany ikony i obsługa "przejść"

--- KtoGdzieKiedy/app/src/main/java/com/example/ktogdziekiedy/MainActivity.kt
+++ KtoGdzieKiedy/app/src/main/java/com/example/ktogdziekiedy/MainActivity.kt
@@ -1,31 +1,35 @@
 package com.example.ktogdziekiedy
 
-import android.content.res.Resources
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var tabLayout: TabLayout
-    private lateinit var viewPager: ViewPager2
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        onClickListener()
+    }
 
-        tabLayout = findViewById(R.id.tabLayout)
-        viewPager = findViewById(R.id.viewPager)
-        viewPager.adapter = MyAdapter(this)
-        TabLayoutMediator(tabLayout, viewPager) { tab, index ->
-            tab.text = when(index) {
-                0 -> {"Wymiana"}
-                1 -> {"Lutowanie"}
-                2 -> {"Czyszczenie"}
-                else -> { throw Resources.NotFoundException("Nie znaleziono pozycji")}
-            }
-        }.attach()
+    public fun onClickListener() {
+        var imageFirst = findViewById<ImageView>(R.id.ivFirst)
+        var imageSecond = findViewById<ImageView>(R.id.ivSecond)
+
+        imageFirst.setOnClickListener {
+            openFirstActivity()
+        }
+
+        imageSecond.setOnClickListener {
+            openSecondActivity()
+        }
+    }
+
+    public fun openFirstActivity() {
+        startActivity(Intent(this@MainActivity, FirstActivity::class.java))
+    }
+
+    public fun openSecondActivity() {
+        startActivity(Intent(this@MainActivity, SecondActivity::class.java))
     }
 }
