a3de142bb3edf4061449dfaa7e9a34b2c86d9ace
ejsiik <72543889+ejsiik@users.noreply.github.com>
2022-12-13 16:02:14 +0100

    Revert "Wstępny wygląd strony"
    This reverts commit 3066274f7d9b917905de6bc7ecae4de9f7ece82d.

--- KtoGdzieKiedy/app/src/main/java/com/example/ktogdziekiedy/FirstActivity.kt
+++ KtoGdzieKiedy/app/src/main/java/com/example/ktogdziekiedy/FirstActivity.kt
@@ -3,7 +3,7 @@ package com.example.ktogdziekiedy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class AddTaskActivity : AppCompatActivity() {
+class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
--- KtoGdzieKiedy/app/src/main/java/com/example/ktogdziekiedy/SecondActivity.kt
+++ KtoGdzieKiedy/app/src/main/java/com/example/ktogdziekiedy/SecondActivity.kt
@@ -3,7 +3,7 @@ package com.example.ktogdziekiedy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class TaskHistoryActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
