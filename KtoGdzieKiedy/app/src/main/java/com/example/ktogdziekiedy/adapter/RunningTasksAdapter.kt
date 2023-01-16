de9c9c79301b20030fb6d198e2306ee8b5321aee
Dawid Wydra <72543889+ejsiik@users.noreply.github.com>
2023-01-16 22:10:26 +0100

    Update RunningTasksAdapter.kt

--- KtoGdzieKiedy/app/src/main/java/com/example/ktogdziekiedy/adapter/RunningTasksAdapter.kt
+++ KtoGdzieKiedy/app/src/main/java/com/example/ktogdziekiedy/adapter/RunningTasksAdapter.kt
@@ -7,17 +7,20 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.NonNull
 import androidx.recyclerview.widget.RecyclerView
+import backendconnection.BackendClient
 import backendconnection.Task
 import com.example.ktogdziekiedy.ItemsViewModel
 import com.example.ktogdziekiedy.R
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
-class RunningTasksAdapter (private val mList: List<Task>): RecyclerView.Adapter<RunningTasksAdapter.ViewHolder>() {
+class RunningTasksAdapter (private var mList: List<Task>): RecyclerView.Adapter<RunningTasksAdapter.ViewHolder>() {
     // Holds the views for adding it to image and text
     class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
         val textView: TextView = itemView.findViewById(R.id.textViewContent)
     }
 
-    var onItemClick: ((Task) -> Unit)? = null
+    var onItemClick: ((String) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         // inflates the card_view_design view
@@ -34,11 +37,19 @@ class RunningTasksAdapter (private val mList: List<Task>): RecyclerView.Adapter<
 
         // sets the text to the textview from our itemHolder class
         holder.textView.text = ItemsViewModel//.toString()
-
-        /*holder.itemView.setOnClickListener {
-            //onItemClick?.invoke(ItemsViewModel)
-            Toast.makeText(holder.itemView.context, "Start task", Toast.LENGTH_LONG).show()
-        }*/
+        val adapter = RunningTasksAdapter(mList)
+        holder.itemView.setOnClickListener {
+            onItemClick?.invoke(ItemsViewModel)
+            GlobalScope.launch {
+                BackendClient.changeTaskStatus(mList[position].id)
+            }.invokeOnCompletion {
+                mList = mList.filter {
+                    it.id != mList[position].id
+                }
+                adapter.notifyItemRemoved(position)
+            }
+            Toast.makeText(holder.itemView.context, "Task "+ mList[position].name+" ended", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun getItemCount(): Int {
